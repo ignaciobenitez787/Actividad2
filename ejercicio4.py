77abc7691ad5cf0eb204531376f5eeff5cc3b526
Ignacio Benitez <ignaciobenitez787@gmail.com>
2025-04-04 13:56:44 -0300

    Modificado ejercicio 4 para utilizar el archivo de funciones

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,7 +1,4 @@
-def check_username(username):
-    """Takes a username and checks if it's valid"""
-    return (len(username) >= 5) and (any(char.isdigit() for char in username)) and (any(char.isupper() for char in username)) and (all(char.isalnum() for char in username))
-
+from funciones import check_username
 user = input("Ingresa un nombre de usuario:")
 if check_username(user):
     print("El nombre de usuario es válido.")
